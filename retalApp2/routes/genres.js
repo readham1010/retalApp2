40faa8b131262930848662f3971dfb04c2fd463f
readham1010 <reema.aladham@hambs.com.au>
2019-12-01 09:43:34 +1030

    add genres route handler

--- retalApp2/routes/genres.js
+++ retalApp2/routes/genres.js
@@ -1,1 +1,12 @@
-﻿
+﻿const { Genre, validate } = require('../models/genre');
+const express = require('express');
+const router = express.Router();
+
+// anyone can list genres. No need to check authontication nor authorization
+router.get('/', async (req, res) => {
+    const genres = await Genre.find().sort('name');
+    if (!genres) res.status(404).send('no genries found');
+    res.send(genres);
+});
+
+module.exports = router;
