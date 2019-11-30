2e557ff1e543d9aa5fc908362cc8500d8a123c7a
readham1010 <reema.aladham@hambs.com.au>
2019-12-01 09:54:38 +1030

    add module to handle Mongodb connection

---
+++ retalApp2/startup/db.js
@@ -0,0 +1,17 @@
+﻿//const winston = require('winston');
+const mongoose = require('mongoose');
+
+module.exports = function () {
+    
+    mongoose.connect('mongodb+srv://readham:White123@cluster0-y7udi.mongodb.net/playground', {// return a promise
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        .then(() => {
+            console.log('Connected to MongoDB')
+        })
+        .catch(err => { // mongoose connection error will be handled here
+            console.error('App starting error:', err.stack)
+        });
+
+}
