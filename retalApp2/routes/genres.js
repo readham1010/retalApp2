aadd5d6849e12baa88be56bed11c3131742a927c
readham1010 <reema.aladham@hambs.com.au>
2019-12-01 09:55:26 +1030

    add defensive programming to check # of genres

--- retalApp2/routes/genres.js
+++ retalApp2/routes/genres.js
@@ -5,7 +5,8 @@ const router = express.Router();
 // anyone can list genres. No need to check authontication nor authorization
 router.get('/', async (req, res) => {
     const genres = await Genre.find().sort('name');
-    if (!genres) res.status(404).send('no genries found');
+    if (!genres) { res.status(404).send('no genries found'); return; }
+    if (genres.length < 1) { res.status(200).send('No Genres'); return; }
     res.send(genres);
 });
 
