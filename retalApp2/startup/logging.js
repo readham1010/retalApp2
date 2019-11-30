b3af936d9f698b18e058e437f8cf28453d3eb8d5
readham1010 <reema.aladham@hambs.com.au>
2019-12-01 08:47:24 +1030

    install winston-mongodb library to log mongoDB actions and errors

--- retalApp2/startup/logging.js
+++ retalApp2/startup/logging.js
@@ -1,1 +1,18 @@
-﻿
+﻿const winston = require('winston');
+require('winston-mongodb');
+require('express-async-errors');
+
+module.exports = function () {
+    winston.handleExceptions(
+        new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
+
+    process.on('unhandledRejection', (ex) => {
+        throw ex;
+    });
+
+    winston.add(winston.transports.File, { filename: 'logfile.log' });
+    winston.add(winston.transports.MongoDB, {
+        db: 'mongodb+srv://<username>:<password>@cluster0-y7udi.mongodb.net/rental',
+        level: 'info'
+    });
+}
