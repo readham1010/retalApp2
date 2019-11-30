7385b60a18e2b9b5dea68f162481ffd3c9a219bd
readham1010 <reema.aladham@hambs.com.au>
2019-12-01 09:58:33 +1030

    add post new Genre rout handler

--- retalApp2/routes/genres.js
+++ retalApp2/routes/genres.js
@@ -10,4 +10,14 @@ router.get('/', async (req, res) => {
     res.send(genres);
 });
 
+router.post('/',  async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let genre = new Genre({ name: req.body.name });
+    genre = await genre.save();
+
+    res.send(genre);
+});
+
 module.exports = router;
