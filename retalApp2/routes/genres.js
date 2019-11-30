336136f5d4bae390a27469f7fc64966246db5f5c
readham1010 <reema.aladham@hambs.com.au>
2019-12-01 10:27:01 +1030

    add API end point to delete a genre by passing genre id

--- retalApp2/routes/genres.js
+++ retalApp2/routes/genres.js
@@ -37,5 +37,11 @@ router.put('/:id', async (req, res) => {
     res.send(genre);
 });
 
+router.delete('/:id',  async (req, res) => {
+    const genre = await Genre.findByIdAndRemove(req.params.id, { useFindAndModify:false});
 
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+    res.send(genre);
+});
 module.exports = router;
