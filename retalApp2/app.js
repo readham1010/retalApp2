5f017baf10408a3c55316a7d2b62b2c090005b88
readham1010 <reema.aladham@hambs.com.au>
2019-12-01 08:17:46 +1030

    create logger use express frameword Initialize the listener

--- retalApp2/app.js
+++ retalApp2/app.js
@@ -1,3 +1,15 @@
 'use strict';
+const { createLogger,  transports } = require('winston');
+const express = require('express');
+const app = express();
 
-console.log('Hello world');
+const logger = createLogger({
+        transports: [new transports.Console()]
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => logger.log({
+    level: 'info',
+    message: `Listening on port ${port}...`
+}));
