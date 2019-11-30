f2d74b4a13d58ba234edba48b38278af23ab00df
readham1010 <reema.aladham@hambs.com.au>
2019-12-01 09:04:00 +1030

    add startup/logging to the listener

--- retalApp2/app.js
+++ retalApp2/app.js
@@ -2,6 +2,7 @@
 const { createLogger,  transports } = require('winston');
 const express = require('express');
 const app = express();
+require('./startup/logging');
 require('./startup/config')();
 
 const logger = createLogger({
