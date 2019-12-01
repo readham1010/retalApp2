bf1906aabac77b52694bf659fc8ceabb98a95046
readham1010 <reema.aladham@hambs.com.au>
2019-12-01 15:55:23 +1030

    add movies route to the listener

--- retalApp2/startup/routes.js
+++ retalApp2/startup/routes.js
@@ -1,9 +1,11 @@
 ﻿const express = require('express');
 const genres = require('../routes/genres');
 const users = require('../routes/users');
+const movies = require('../routes/movies');
 
 module.exports = function (app) {
     app.use(express.json());
     app.use('/api/genres', genres);
     app.use('/api/users', users);
+    app.use('/api/movies', movies);
 }
