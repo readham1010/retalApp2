a5d7fb612ff62f9487dfa1d932aa90b1a4f906f4
readham1010 <reema.aladham@hambs.com.au>
2019-12-01 16:15:32 +1030

    add customers to the routes

--- retalApp2/startup/routes.js
+++ retalApp2/startup/routes.js
@@ -2,10 +2,11 @@
 const genres = require('../routes/genres');
 const users = require('../routes/users');
 const movies = require('../routes/movies');
-
+const customers = require('../routes/customers');
 module.exports = function (app) {
     app.use(express.json());
     app.use('/api/genres', genres);
     app.use('/api/users', users);
     app.use('/api/movies', movies);
+    app.use('/api/customers', customers);
 }
