56a46daedb5701b17d782580d31db17dac8fb2a9
readham1010 <reema.aladham@hambs.com.au>
2019-12-01 12:22:26 +1030

    add users api to the routes

--- retalApp2/startup/routes.js
+++ retalApp2/startup/routes.js
@@ -1,8 +1,9 @@
 ﻿const express = require('express');
 const genres = require('../routes/genres');
+const users = require('../routes/users');
 
 module.exports = function (app) {
     app.use(express.json());
     app.use('/api/genres', genres);
-   
+    app.use('/api/users', users);
 }
