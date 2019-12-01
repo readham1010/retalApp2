7a6e00e83469d8f5151c537715e011d2f04d02af
readham1010 <reema.aladham@hambs.com.au>
2019-12-01 15:54:58 +1030

    add movies route handler with the following API endpoints 1- Get all 2- Post a movie using denormalization 3- Update a movie 4- delete a movie 5- Get a movie by id

---
+++ retalApp2/routes/movies.js
@@ -0,0 +1,76 @@
+﻿const { Movie, validate } = require('../models/movie');
+const { Genre } = require('../models/genre');
+const { validateId } = require('../models/id');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const movies = await Movie.find().sort('name');
+    res.send(movies);
+});
+
+//denormalization.
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = await Genre.findById(req.body.genreId);
+    if (!genre) return res.status(400).send('Invalid genre.');
+
+    const movie = new Movie({
+        title: req.body.title,
+        genre: {
+            _id: genre._id,
+            name: genre.name
+        },
+        numberInStock: req.body.numberInStock,
+        dailyRentalRate: req.body.dailyRentalRate
+    });
+    await movie.save();
+
+    res.send(movie);
+});
+
+router.put('/:id', async (req, res) => {
+    if (!validateId(req.params.id)) return res.status(400).send("Invalid object id");
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = await Genre.findById(req.body.genreId);
+    if (!genre) return res.status(400).send('Invalid genre.');
+
+    const movie = await Movie.findByIdAndUpdate(req.params.id,
+        {
+            title: req.body.title,
+            genre: {
+                _id: genre._id,
+                name: genre.name
+            },
+            numberInStock: req.body.numberInStock,
+            dailyRentalRate: req.body.dailyRentalRate
+        }, { new: true, useFindAndModify: false});
+
+    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+
+    res.send(movie);
+});
+
+router.delete('/:id', async (req, res) => {
+    if (!validateId(req.params.id)) return res.status(400).send("Invalid object id");
+    const movie = await Movie.findByIdAndRemove(req.params.id, { useFindAndModify: false });
+
+    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+
+    res.send(movie);
+});
+
+router.get('/:id', async (req, res) => {
+    if (!validateId(req.params.id)) return res.status(400).send("Invalid object id");
+    const movie = await Movie.findById(req.params.id);
+
+    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+
+    res.send(movie);
+});
+
+module.exports = router; 
