af3300bbcba86b02e3977ead264ad27592df53ae
readham1010 <reema.aladham@hambs.com.au>
2019-12-01 12:04:59 +1030

    Add id model to validate user's passed object id in put genres API endpoint

--- retalApp2/routes/genres.js
+++ retalApp2/routes/genres.js
@@ -1,7 +1,9 @@
 ﻿const auth = require('../middleware/auth');
 const { Genre, validate } = require('../models/genre');
+const { validateId } = require('../models/id');
 const express = require('express');
 const router = express.Router();
+
 
 // anyone can list genres. No need to check authontication nor authorization
 router.get('/', async (req, res) => {
@@ -12,7 +14,7 @@ router.get('/', async (req, res) => {
 });
 
 // only authonticated users can add new Genres
-router.post('/', auth, async (req, res) => {
+router.post('/',  async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
@@ -24,6 +26,9 @@ router.post('/', auth, async (req, res) => {
 
 // only authonticated users can update current Genres
 router.put('/:id', async (req, res) => {
+    
+    if (!validateId(req.params.id)) return res.status(400).send("Invalid object id");
+
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
@@ -39,6 +44,14 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id',  async (req, res) => {
     const genre = await Genre.findByIdAndRemove(req.params.id, { useFindAndModify:false});
+
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+    res.send(genre);
+});
+
+router.get('/:id', async (req, res) => {
+    const genre = await Genre.findById(req.params.id);
 
     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
