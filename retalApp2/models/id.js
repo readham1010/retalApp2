f81c40147a564a5f41f2e3c73808daceda50a5ac
readham1010 <reema.aladham@hambs.com.au>
2019-12-01 12:04:00 +1030

    add id model as a defencive programming to validate user's passed object id

---
+++ retalApp2/models/id.js
@@ -0,0 +1,14 @@
+﻿const mongoose = require('mongoose');
+
+
+
+function validateId(id) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return false;
+    }
+
+    return true;
+}
+
+
+exports.validateId = validateId;
