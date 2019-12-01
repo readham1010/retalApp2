c2be8b60efee724be3e9b0229ffc18d08ee1b169
readham1010 <reema.aladham@hambs.com.au>
2019-12-01 16:31:06 +1030

    add the rental handler to the routes

--- retalApp2/startup/routes.js
+++ retalApp2/startup/routes.js
@@ -3,10 +3,12 @@ const genres = require('../routes/genres');
 const users = require('../routes/users');
 const movies = require('../routes/movies');
 const customers = require('../routes/customers');
+const rentals = require('../routes/rentals');
 module.exports = function (app) {
     app.use(express.json());
     app.use('/api/genres', genres);
     app.use('/api/users', users);
     app.use('/api/movies', movies);
     app.use('/api/customers', customers);
+    app.use('/api/rentals', rentals);
 }
